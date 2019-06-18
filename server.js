afa0781603e5892608cd516a03f8590b0a1cdff0
ruganda <mubaruganda@gmail.com>
2019-06-18 11:44:04 +0300

    set up dev env

---
+++ server.js
@@ -0,0 +1,17 @@
+
+import express from 'express'
+import path from 'path';
+import open  from 'open';
+
+const port = 5000;
+const app = express();
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, './client/index.html'))
+})
+app.listen(port, function(err){
+  if(err){
+    console.log(err)
+  }else{
+    open('http://localhost:'+port)
+  }
+});
