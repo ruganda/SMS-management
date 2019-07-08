174a053b35ff56acd8cc1527d1282e62c96d2313
ruganda <mubaruganda@gmail.com>
2019-07-08 17:29:58 +0300

    test users

--- tests/test.js
+++ tests/test.js
@@ -1,18 +1,22 @@
 import app from '../server.js';
 import request from 'supertest';
+import models from "../models/index";
 const expect = require('chai').expect;
 
-describe('Test user routes', ()=>{
+// models.User.sync({ force: true })
+// models.Message.sync({force:true})
 
+describe('Test user routes', ()=>{
+  models.User.sync({ force: false })
   it('should add a contact', (done)=>{
     request(app).post('/user')
     .send({
-      "name": "Mubarak",
+      "name": "Test user",
       "phoneNumber": "0779850746"
     })
    .end((err, res)=>{
     expect(res.status).to.equal(200)
-    expect(res.body.user.name).to.equal("Mubarak")
+    expect(res.body.user.name).to.equal("Test user")
     done();
    })
    
