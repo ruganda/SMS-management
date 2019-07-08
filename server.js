766b9850f41eba8f1df0ec0c95bad0f298e8223f
ruganda <mubaruganda@gmail.com>
2019-07-09 02:40:54 +0300

    add port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import models from './models/index';
 import { bodyParser } from 'json-server';
 import Routes from './routers'
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(bodyParser);
